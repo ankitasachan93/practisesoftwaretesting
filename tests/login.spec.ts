8dafd2af1a73320007a9120ee08cba1f0e92e457
Ankita Sachan <ankitasachan93@gmail.com>
2026-04-20 10:51:56 +1200

    added snapshot code and wait

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -23,32 +23,52 @@ test.describe('Login Flow' , () =>{
         const password = 'A@nkita1234512345'
         await registerUser(request, email, password);
         await loginpage.login(email,password);
-        expect( page.locator('[data-test="nav-menu"]')).toBe("Ankita Sachan")
+        await expect(page.getByTestId("nav-menu")).toContainText("Ankita Sachan");
 
     })
 
-    test.describe('Home page with customer01 no auth', () =>{
+    test.describe('Home page search for Thor Hammer', () =>{
         test('Search assertion', async({page})=>{
-        //Search for Thor Hammer
-        await page.goto("https://practicesoftwaretesting.com/");
-        await page.locator('[data-test="search-query"]').fill("Thor Hammer");
-        await page.locator('[data-test="search-submit"]').click()
-
-
-        //Check the result in grid(assertion)
-        expect( page.locator('h5:has-text("Thor Hammer")'))
+            
+            await page.goto("https://practicesoftwaretesting.com/")
+            
+            const productGrid = page.locator(".col-md-9");
+            await page.getByTestId("search-query").fill("Thor Hammer");
+            await page.getByTestId("search-submit").click();
+            await expect(productGrid.getByRole("link")).toHaveCount(7);
+            await expect(page.getByAltText("Thor Hammer")).toBeVisible();
     })
                        
+    })
+
+    test.describe("Navigating to landing page", () =>{
+
+        test("visual Test ", async({page})=>{
+            await page.waitForLoadState("networkidle")
+            await expect(page).toHaveScreenshot("Home-page-no-auth.png") //screenshot block
+        })
+
+        test("Login with no auth", async({page})=>{
+            await page.goto("https://practicesoftwaretesting.com/")
+        });
+
     })
 
     test.describe("Home page with customer01 with auth", () =>{
         test.use({storageState: ".auth/customer01.json"});
         test.beforeEach(async({page})=>{
+
             await page.goto("https://practicesoftwaretesting.com/");
+            
+        })
+        test("visual Test ", async({page})=>{
+            await page.waitForLoadState("networkidle")
+            await expect(page).toHaveScreenshot("Home-page-auth.png") //screenshot block
         })
 
         test("check the customer01 is signed in", async({page})=>{
             await expect(page.getByTestId("nav-sign-in")).not.toBeVisible(); 
+            await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
 
         })
     })
